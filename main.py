138fdd0e6b7bb068fabf57a9d872c63c263c729f
Daniel Ayoub <daniel.ayoub@rutgers.edu>
2016-02-13 16:33:38 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+!#/usr/bin/env python
+import mavlink
+import serial
+
+radio = serial.Serial()
+whoi = serial.Serial()
+apm = serial.Serial()
+
+radio.baudrate = 57600
+whoi.baudrate = 9600
+apm.baudrate = 57600
+
+radio.port = '/dev/ttyUSB0'
+whoi.port = '/dev/ttyUSB1'
+apm.port = '/dev/ttyUSB2'
+
+radio.open()
+whoi.open()
+apm.open()
